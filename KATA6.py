0f1e168020a3d722e737e1be7b92e02b49bbe364
Zoey Kim <zoooey96@gmail.com>
2024-06-09 21:51:26 -0700

    add some data

--- KATA6.py
+++ KATA6.py
@@ -10,38 +10,3 @@ def whereCanIPark(spots, vehicle):
                 return [x, y]
                 
     return False
-
-
-print(whereCanIPark(
-  [
-    ['s', 's', 's', 'S', 'R', 'M'],
-    ['s', 'M', 's', 'S', 'r', 'M'],
-    ['s', 'M', 's', 'S', 'r', 'm'],
-    ['S', 'r', 's', 'm', 'r', 'M'],
-    ['S', 'r', 's', 'm', 'r', 'M'],
-    ['S', 'r', 'S', 'M', 'M', 'S']
-  ],
-  'regular'
-))
-
-print(whereCanIPark(
-  [
-    ['M', 'M', 'M', 'M'],
-    ['M', 's', 'M', 'M'],
-    ['M', 'M', 'M', 'M'],
-    ['M', 'M', 'r', 'M']
-  ],
-  'small'
-)) 
-
-print(whereCanIPark(
-  [
-    ['s', 's', 's', 's', 's', 's'],
-    ['s', 'm', 's', 'S', 'r', 's'],
-    ['s', 'm', 's', 'S', 'r', 's'],
-    ['S', 'r', 's', 'm', 'r', 's'],
-    ['S', 'r', 's', 'm', 'R', 's'],
-    ['S', 'r', 'S', 'M', 'm', 'S']
-  ],
-  'motorcycle'
-))
