c80b15acea6bbecc1c6bcc8054b3d5b737495759
Zoey Kim <zoooey96@gmail.com>
2024-06-23 23:59:12 -0700

    fix error

--- KATA1.py
+++ KATA1.py
@@ -3,7 +3,3 @@ def sumLargestNumbers(numbers):
     numbers.remove(largest)
     second_largest = max(numbers)
     return largest + second_largest
-
-print(sumLargestNumbers([1, 10])) 
-print(sumLargestNumbers([1, 2, 3]))
-print(sumLargestNumbers([10, 4, 34, 6, 92, 2]))
