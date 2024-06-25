e121de7240e847c46afcc3303399add7c7ba57f4
Zoey Kim <zoooey96@gmail.com>
2024-06-24 23:51:11 -0700

    fix error

--- KATA4.py
+++ KATA4.py
@@ -7,3 +7,9 @@ print(instructorWithLongestName([
   {"name": "Ophilia", "course": "Web"},
   {"name": "Donald", "course": "Web"}
 ]))  
+
+print(instructorWithLongestName([
+  {"name": "Matthew", "course": "Data"},
+  {"name": "David", "course": "iOS"},
+  {"name": "Domascus", "course": "Web"}
+]))
