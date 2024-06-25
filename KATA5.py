17f050b4f3f023fe2a8a5c45739f4e8e0f6ce63f
Zoey Kim <zoooey96@gmail.com>
2024-06-24 23:51:34 -0700

    fix error

--- KATA5.py
+++
@@ -1,8 +0,0 @@
-def url_encode(text):
-    encoded_text = []
-    for char in text.strip():
-        if char == ' ':
-            encoded_text.append('%20')
-        else:
-            encoded_text.append(char)
-    return ''.join(encoded_text)
