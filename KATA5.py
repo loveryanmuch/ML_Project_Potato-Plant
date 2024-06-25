4afd432d21fe26f423fa8b97df70abbbfe959658
Zoey Kim <zoooey96@gmail.com>
2024-06-24 23:51:55 -0700

    add functions

---
+++ KATA5.py
@@ -0,0 +1,8 @@
+def url_encode(text):
+    encoded_text = []
+    for char in text.strip():
+        if char == ' ':
+            encoded_text.append('%20')
+        else:
+            encoded_text.append(char)
+    return ''.join(encoded_text)
